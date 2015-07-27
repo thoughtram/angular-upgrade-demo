fdcdd67bce75472cf3cd88fb8620c62ec1b9c100
Pascal Precht <pascal.precht@googlemail.com>
2015-07-27 11:17:29 +0200

    step-http

--- app.js
+++ app.js
@@ -1,96 +1,22 @@
 var app = angular.module('SampleApp', []);
 
-app.controller('PostsController', function (PostsService, UsersService, PhotosService) {
+app.value('config', {
+  apiUrl: 'http://restcountries.eu/rest/v1'
+});
+
+app.controller('CountriesController', function (CountriesService) {
   var that = this;
-
-  PostsService.getPosts().then(function (posts) {
-    that.posts = posts;
+  
+  CountriesService.getCountries().then(function (countries) {
+    console.dir(countries);
+    that.countries = countries;
   });
-
-  this.getUserByPost = UsersService.getUserByPost;
-  this.getPhotoByPost = PhotosService.getPhotoByPost;
 });
 
-app.service('PostsService', function ($q) {
-  var posts = [
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    }
-  ];
-
-  this.getPosts = function () {
-    return $q(function (resolve) {
-      resolve(posts);
+app.service('CountriesService', function ($http, config) {
+  this.getCountries = function () {
+    return $http.get(config.apiUrl + '/all').then(function (response) {
+      return response.data;
     });
-  };
-});
-
-app.service('UsersService', function () {
-  var users = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz"
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv",
-    }
-  ];
-
-  this.getUserByPost = function (post) {
-    return users.filter(function (user) {
-      return user.id === post.userId;
-    })[0];
-  };
-});
-
-app.service('PhotosService', function () {
-  var photos = [
-    {
-      "albumId": 1,
-      "id": 1,
-      "title": "accusamus beatae ad facilis cum similique qui sunt",
-      "url": "http://placehold.it/600/92c952",
-      "thumbnailUrl": "http://placehold.it/150/30ac17"
-    },
-    {
-      "albumId": 1,
-      "id": 2,
-      "title": "reprehenderit est deserunt velit ipsam",
-      "url": "http://placehold.it/600/771796",
-      "thumbnailUrl": "http://placehold.it/150/dff9f6"
-    },
-    {
-      "albumId": 1,
-      "id": 3,
-      "title": "officia porro iure quia iusto qui ipsa ut modi",
-      "url": "http://placehold.it/600/24f355",
-      "thumbnailUrl": "http://placehold.it/150/1941e9"
-    }
-  ];
-
-  this.getPhotoByPost = function (post) {
-    return photos.filter(function (photo) {
-      return photo.id === post.id;
-    })[0];
   };
 });

878fc2d20de1773f7b3a1661dc0bbbb84e9cd8ad
Pascal Precht <pascal.precht@googlemail.com>
2015-07-27 12:14:55 +0200

    step-routing

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var app = angular.module('SampleApp', []);
+var app = angular.module('SampleApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'CountriesController as ctrl',
+      templateUrl: 'countries.html'
+    })
+    .when('/:country', {
+      controller: 'CountryController as ctrl',
+      templateUrl: 'country.html'
+    })
+    .otherwise('/');
+});
 
 app.value('config', {
   apiUrl: 'http://restcountries.eu/rest/v1'
@@ -6,17 +19,40 @@ app.value('config', {
 
 app.controller('CountriesController', function (CountriesService) {
   var that = this;
-  
+
   CountriesService.getCountries().then(function (countries) {
     console.dir(countries);
     that.countries = countries;
   });
 });
 
-app.service('CountriesService', function ($http, config) {
+app.service('CountriesService', function ($http, config, $q) {
+  var countries = [];
+
   this.getCountries = function () {
-    return $http.get(config.apiUrl + '/all').then(function (response) {
-      return response.data;
+    return $q(function (resolve, reject) {
+      if (countries.length > 0) {
+        resolve(countries);
+      } else {
+        resolve($http.get(config.apiUrl + '/all').then(function (response) {
+          countries = response.data;
+          return countries;
+        }));
+      }
     });
   };
+
+  this.getCountry = function (name) {
+    return $http.get(config.apiUrl + '/name/' + name).then(function (response) {
+      return response.data[0];
+    });
+  };
+});
+
+app.controller('CountryController', function (CountriesService, $routeParams) {
+  var that = this;
+  CountriesService.getCountry($routeParams.country).then(function (country) {
+    console.dir(country);
+    that.country = country;
+  });
 });

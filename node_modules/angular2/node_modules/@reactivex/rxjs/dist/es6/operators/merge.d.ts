a61a71c933d58894ac4ccfb172d987d7ac9072ba
Brett Byler <bbyler12@gmail.com>
2015-10-30 10:30:12 -0700

    Removing node_modules from version control

--- node_modules/angular2/node_modules/@reactivex/rxjs/dist/es6/operators/merge.d.ts
+++
@@ -1,2 +0,0 @@
-import Observable from '../Observable';
-export default function merge<R>(...observables: (Observable<any> | number)[]): Observable<R>;
--- node_modules/angular2/node_modules/@reactivex/rxjs/dist/es6/subjects/GroupSubject.d.ts
+++
@@ -1,5 +0,0 @@
-import Subject from '../Subject';
-export default class GroupSubject<T> extends Subject<T> {
-    key: string;
-    constructor(key: string);
-}
